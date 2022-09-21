03434f1776aefe1b261f674a9e60322987b99bd9
calher99 <calher99@gmail.com>
2022-09-21 13:19:28 +0100

    create function with algorithms

--- script.js
+++ script.js
@@ -16,29 +16,43 @@
 
 //User provides choice
 
-    let userChoice = prompt("Please enter your choice:", "rock");
+    let userChoice =  prompt("Please enter your choice:", "rock").toLowerCase();
 
 console.log(computerChoice);
 console.log(userChoice);
+
 //Computer checks algorithm who wins
 
-if (computerChoice === userChoice){
-    console.log("draw");
-} else if (computerChoice==="rock" && userChoice==="scissors"){
-    console.log("you lost");
-}  else if (computerChoice==="paper" && userChoice==="scissors"){
-    console.log("you won");
-}  else if (computerChoice==="rock" && userChoice==="paper"){
-    console.log("you won");
-}  else if (computerChoice==="paper" && userChoice==="rock"){
-    console.log("you lost");
-}  else if (computerChoice==="scissors" && userChoice==="rock"){
-    console.log("you won");
-}    else if (computerChoice==="scissors" && userChoice==="paper"){
-        console.log("you lost");
+let winner = (user1_result,user2_result) =>{
+
+    if (user1_result === user2_result){
+        return 2;
+    }else if (user1_result==="rock" && user2_result==="scissors"){
+        return 1;
+    }else if (user1_result==="paper" && user2_result==="scissors"){
+        return 0;
+    }else if (user1_result==="rock" && user2_result==="paper"){
+        return 0;
+    }else if (user1_result==="paper" && user2_result==="rock"){
+        return 1;
+    }else if (user1_result==="scissors" && user2_result==="rock"){
+        return 0;
+    }else if (user1_result==="scissors" && user2_result==="paper"){
+        return 1;
+    }
+
 }
 
 //Provide answer
 
+let printWinner = value => {
+    if (value === 1){
+        return "You won!";
+    }else if (value === 0){
+        return "You lost";
+    } else{
+        return "Draw";
+    }
+}
 
-
+console.log(printWinner(winner(userChoice,computerChoice)));
